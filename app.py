a218b43bb097eff0c8afa978f9b006af0789f98d
OctavioGov1 <ojgpdeveloper@gmail.com>
2020-06-07 18:14:49 -0500

    Parte sin excel
    Brevemente:
    -Se agrega un archivo nuevo de operaciones para mejor organizacion.
    -Se agrega un HTML de error, del sin Excel y su resultado.

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 import pandas as pd
 import operaciones as op
+import operacionesSinExcel as opse
 
 app = Flask(__name__)
 
@@ -12,9 +13,48 @@ def inicio():
 def teoria():
     return render_template('teoria.html')
 
+@app.route('/sinExcel')
+def sinExcel():
+    return render_template('sinExcel.html')
+
 @app.route('/subirExcel')
 def subirExcel():
     return render_template('subirExcel.html')
+
+@app.route('/datosSinExcel', methods=['POST'])
+def datosSinExcel():
+    t_muestra = request.form['t_muestra'];
+    media = request.form['media'];
+    varianza_m = request.form['varianza_m'];
+    varianza_p = request.form['varianza_p'];
+    alfa = request.form['inlineRadioOptions'];
+    print("El alfa fue"+alfa)
+    #Primero checo que no esten ambos llenos o vacios, es prioridad esta condicion, solo uno de los dos (el caso de "else") debe estar lleno
+    if((vacio(varianza_m)==False and vacio(varianza_p)==False) or (vacio(varianza_m)==True and vacio(varianza_p)==True)):
+        return render_template('Error.html')
+    else:
+        #Verifico que los demas no esten vacios
+        if(permiso(t_muestra,media,alfa)==True):
+            #hay que convertirlos a numeros, son strings
+            resultado=opse.decision(t_muestra,media,varianza_m,varianza_p,alfa)
+            return render_template('resultadoSinExcel.html',intervalo=resultado)
+        else:
+            return render_template('Error.html')
+
+def permiso(t_muestra,media,alfa):
+    if(vacio(t_muestra)==True or vacio(media)==True or vacio(alfa)==True):
+        return False;
+    else:
+        return True;
+
+def vacio(string):
+    if(string==""):
+        return True
+    else:
+        if(string.isspace()==True):
+            return True
+        else:
+            return False
 
 @app.route('/data', methods=['GET', 'POST'])
 def data():
